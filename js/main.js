fe4d61f58a6ca16ab59d5b95180f840548294141
DESKTOP-78AETGU\Rinatoru <rafi180804@gmail.com>
2022-11-28 09:31:12 +0700

    commitan ke empat

--- js/main.js
+++ js/main.js
@@ -16,22 +16,41 @@
     new WOW().init();
 
 
-    // Fixed Navbar
-    $(window).scroll(function () {
-        if ($(window).width() < 992) {
-            if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-white shadow');
-            } else {
-                $('.fixed-top').removeClass('bg-white shadow');
-            }
-        } else {
-            if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-white shadow').css('top', -45);
-            } else {
-                $('.fixed-top').removeClass('bg-white shadow').css('top', 0);
-            }
+   // Sticky Navbar
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+        $('.navbar').addClass('sticky-top');
+    } else {
+        $('.navbar').removeClass('sticky-top');
+    }
+});
+
+// Dropdown on mouse hover
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+    if (this.matchMedia("(min-width: 992px)").matches) {
+        $dropdown.hover(
+        function() {
+            const $this = $(this);
+            $this.addClass(showClass);
+            $this.find($dropdownToggle).attr("aria-expanded", "true");
+            $this.find($dropdownMenu).addClass(showClass);
+        },
+        function() {
+            const $this = $(this);
+            $this.removeClass(showClass);
+            $this.find($dropdownToggle).attr("aria-expanded", "false");
+            $this.find($dropdownMenu).removeClass(showClass);
         }
-    });
+        );
+    } else {
+        $dropdown.off("mouseenter mouseleave");
+    }
+});
     
     
     // Back to top button
@@ -110,6 +129,29 @@
         }
     });
 
+    // Vendor carousel
+  $(".vendor-carousel").owlCarousel({
+    loop: true,
+    margin: 45,
+    dots: false,
+    loop: true,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      576: {
+        items: 4,
+      },
+      768: {
+        items: 6,
+      },
+      992: {
+        items: 8,
+      },
+    },
+  });
     
 })(jQuery);
 
